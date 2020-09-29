0d9fabf79f23ab82c160d32ed2566cbda1082daf
Dan Untenzu <mail@pixelbrackets.de>
2020-09-29 21:53:15 +0200

    [FEATURE] ✨ Set User Agent
    Set a custom user agent for all requests to help target servers
    identify the request.
    
    See https://twitter.com/seldaek/status/1308414852885512200

--- src/Notification.php
+++ src/Notification.php
@@ -15,7 +15,7 @@ class Notification
         $requestURI = (string) getenv('GIVENOTICE_URI');
 
         try {
-            (new \GuzzleHttp\Client(['timeout' => 1]))->get($requestURI);
+            (new \GuzzleHttp\Client(['timeout' => 1, 'headers' => ['User-Agent' => 'GiveNoticeClient']]))->get($requestURI);
         } catch (GuzzleException $e) {
             // stay silent
         }
